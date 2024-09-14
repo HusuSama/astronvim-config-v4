a9fba020c5bcb88bc0a41389b78e006aebedb46d
1538379200 <1538379200@qq.com>
2024-09-14 10:10:20 +0800

    add themes

--- lua/community.lua
+++ lua/community.lua
@@ -7,34 +7,39 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  -- { import = "astrocommunity.colorscheme.rose-pine" },
+  -- { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  -- pack of languages server
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  { import = "astrocommunity.motion.flash-nvim" },
-  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  -- editing-support
   { import = "astrocommunity.editing-support.multicursors-nvim" },
   { import = "astrocommunity.editing-support.comment-box-nvim" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  -- { import = "astrocommunity.syntax.hlargs-nvim" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  -- motion
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.vim-matchup" },
-  { import = "astrocommunity.editing-support.vim-move" },
+  -- test
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.neotest" },
+  -- completion
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  -- runner
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  -- others
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
   { import = "astrocommunity.programming-language-support.nvim-jqx" },
-  -- { import = "astrocommunity.color.twilight-nvim"},
-  { import = "astrocommunity.motion.marks-nvim" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
-  { import = "astrocommunity.colorscheme.rose-pine" },
-  { import = "astrocommunity.colorscheme.vscode-nvim" },
-  { import = "astrocommunity.test.neotest" },
-  { import = "astrocommunity.code-runner.overseer-nvim" },
   { import = "astrocommunity.register.nvim-neoclip-lua" },
-  { import = "astrocommunity.completion.cmp-cmdline" },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.rust"}
 }
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -12,7 +12,10 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "catppuccin-frappe",
+    -- colorscheme = "catppuccin-frappe",
+    -- colorscheme = "rose-pine",
+    -- colorscheme = "onedark_vivid",
+    colorscheme = "tokyonight-storm",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
